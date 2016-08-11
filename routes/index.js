802033c87bb3723b446892e527b5e76bf8a0e5fe
Jeff Dickey <dickeyxxx@gmail.com>
2016-08-10 20:32:29 -0700

    stop showing headers

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ const r = require('express').Router()
 const path = require('path')
 
 r.get('*', function (req, res) {
-  console.dir(req.headers)
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
 })
 
