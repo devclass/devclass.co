ed41d61dcfca4194308f32e5f9100b68fc5a282e
Rob Vella <me@robvella.com>
2016-08-23 17:34:39 -0700

    Add nodemon to replace browsersync's builtin server

--- gulp/browserSync.js
+++ gulp/browserSync.js
@@ -1,18 +1,30 @@
 // Configure the browserSync task
 
 const gulp = require('gulp')
-const modRewrite  = require('connect-modrewrite');
+const nodemon = require('gulp-nodemon');
+const DEV_PORT = 3000
+const NODE_PORT = 3001
 
 module.exports = (browserSync) => {
-  gulp.task('browserSync', () => {
+  gulp.task('browserSync', ['nodemon'], () => {
     browserSync.init({
-      server: {
-        baseDir: 'public',
-        middleware: [
-          modRewrite([
-            '!\\.\\w+$ /index.html [L]'
-          ])
-        ]
+      proxy: 'http://localhost:' + NODE_PORT + '',
+      files: ['public/**/*.*'],
+      port: DEV_PORT
+    })
+  })
+
+  gulp.task('nodemon', (cb) => {
+    var started = false
+    return nodemon({
+      script: 'server.js',
+      env: {
+        'PORT': NODE_PORT
+      }
+    }).on('start', () => {
+      if (!started) {
+        cb()
+        started = true
       }
     })
   })
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,35 @@
 
 const r = require('express').Router()
 const path = require('path')
+const helper   = require('sendgrid').mail
 
 r.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
 })
+
+r.post('/contact', function (req, res) {
+  let sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+  let from_email = new helper.Email("contact@devclass.net")
+  let to_email = new helper.Email("me@robvella.com")
+  let subject = "Sending with SendGrid is Fun"
+  let content = new helper.Content("text/plain", "and easy to do anywhere, even with Node.js")
+  let mail = new helper.Mail(from_email, subject, to_email, content)
+
+  let request = sg.emptyRequest({
+    method: 'POST',
+    path: '/v3/mail/send',
+    body: mail.toJSON()
+  })
+
+  sg.API(request, function(error, response) {
+    console.log(response.statusCode)
+    console.log(response.body)
+    console.log(response.headers)
+  })
+
+  res.send('hehe')
+  //res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+})
+
 
 module.exports = r
